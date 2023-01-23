550f841e58dc7f3f5c7c18d9269af2510f1524c8
mingi <mkultra0115@gmail.com>
2023-01-24 00:00:12 +0900

    moyora 시작화면 생성 및 방생성 컨트롤러 생성

--- src/main/java/com/project/side/moyora/controller/MoyoraController.java
+++ src/main/java/com/project/side/moyora/controller/MoyoraController.java
@@ -1,15 +1,19 @@
-package com.project.side.moyora;
+package com.project.side.moyora.controller;
 
 import com.project.side.range.mapper.rangeMapper;
 import java.util.HashMap;
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.web.ServerProperties.Reactive.Session;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -18,8 +22,18 @@ public class MoyoraController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     @GetMapping(value = "/moyo")
-    public String moyoraPage(){
+    public String moyoraPage(Model model, @RequestParam(name = "roomNo", required = false) String roomNo){
+        boolean hasRoomNo = roomNo != null;
+        model.addAttribute("hasRoomNo", hasRoomNo);
         return "/moyora/moyo";
     }
 
+
+    @ResponseBody
+    @PostMapping(value = "/room/create")
+    public HashMap<String, String> createRoom(HttpSession mySession){
+        HashMap<String, String> result = new HashMap<>();
+        System.out.println(mySession.getId());
+        return result;
+    }
 }
